950ab8f3116a8143b508b2fcd8434e9dc9f52bd1
sosana <thesosana@gmail.om>
2022-04-25 13:45:45 -0400

    pushing up prelim project

---
+++ formatWealth/app.js
@@ -0,0 +1,6 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const ShowMillionairesBtn = document.getElementById('show-millionares');
+const sortBtn = document.getElementById('main');
+const calcWealthBtn = document.getElementById('main');
