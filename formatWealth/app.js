a7a8474e33a7483de8735d9d3177b9da991e6d3f
sosana <thesosana@gmail.com>
2022-04-25 14:23:35 -0400

    setting up fetch api

--- formatWealth/app.js
+++ formatWealth/app.js
@@ -1,6 +1,18 @@
 const main = document.getElementById('main');
 const addUserBtn = document.getElementById('add-user');
 const doubleBtn = document.getElementById('double');
-const ShowMillionairesBtn = document.getElementById('show-millionares');
-const sortBtn = document.getElementById('main');
-const calcWealthBtn = document.getElementById('main');
+const ShowMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calcWealthBtn = document.getElementById('calculate-wealth');
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+async function getRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+
+  const user = data.results[0];
+  console.log(user);
+}
